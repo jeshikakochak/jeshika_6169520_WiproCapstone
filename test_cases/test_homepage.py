706c8e929dc5e27282a85eb1bfdf505742d0a1c1
jeshikakochak <jeshikakochak@gmail.com>
2026-05-20 23:09:56 +0530

    all commits

--- test_cases/test_homepage.py
+++
@@ -1,7 +0,0 @@
-def test_myntra_homepage_open(driver):
-
-    # Open Myntra website
-    driver.get("https://www.myntra.com")
-
-    # Validate homepage opened successfully
-    assert "myntra" in driver.current_url.lower()
